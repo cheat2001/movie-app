d9079acbc30321030375b117917b0d466537abe2
cheat2001 <83935278+cheat2001@users.noreply.github.com>
2022-06-23 14:43:04 +0700

    style the header section

--- components/Header.js
+++ components/Header.js
@@ -4,16 +4,17 @@ import HeaderIcon from './HeaderIcon'
 import { HomeIcon,UserIcon,PhoneIcon,InformationCircleIcon } from '@heroicons/react/solid'
 export default function Header() {
   return (
-    <div>
-       <div className="">
+    <div className='bg-gray-700 text-gray-200 flex flex-col justify-between items-center p-6 select-none sm:flex-row' >
+       <div className="flex ">
            <HeaderIcon Icon={HomeIcon} title="HOME"/>
            <HeaderIcon Icon={UserIcon} title="Account"/>
            <HeaderIcon Icon={PhoneIcon} title="Contact"/>
            <HeaderIcon Icon={InformationCircleIcon} title="About"/>
        </div>
-       <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/640px-IMDB_Logo_2016.svg.png"
+       <Image  src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/640px-IMDB_Logo_2016.svg.png"
        width={100}
         height={50}
+        className="cursor-pointer active:brightness-110"
        />
     </div>
   )
--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function HeaderIcon({Icon,title}) {
   return (
-    <div>
-        <Icon className="h-8 "/>
-        <p>{title}</p>
+    <div className='mx-4 flex flex-col items-center p-5 hover:opacity-70 active:text-red-500 cursor-pointer lg:mx-6'>
+        <Icon className="h-8"/>
+        <p className='my-2 '>{title}</p>
     </div>
   )
 }
